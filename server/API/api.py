09717c97b16c5740115e2f4f5a89694f84e22785
MishaSok <mikhail.sokov2006@gmail.com>
2022-04-03 06:15:23 +0200

    - API updated (/api/profile/delete) POST

--- server/API/api.py
+++ server/API/api.py
@@ -3,6 +3,7 @@ from flask import request
 
 from models import db_session
 from models import users
+from config_server import super_user_token
 
 blueprint = flask.Blueprint(
     'api',
@@ -11,19 +12,52 @@ blueprint = flask.Blueprint(
 )
 
 
+# Получить информацию о пользователе по id
 @blueprint.route('/api/profile/<int:profile_id>', methods=['GET'])
-def get_one_news(profile_id):
+def get_profile_info(profile_id):
     if request.method == 'GET':
-        db_sess = db_session.create_session()
-        news = db_sess.query(users.User).get(profile_id)
-        if not news:
-            return flask.jsonify({'error': 'Not found'})
-        return flask.jsonify(
-            {
-                'profile': news.to_dict(only=(
-                    'id', 'username', 'about', 'email'))
-            }
-        )
+        try:
+            db_sess = db_session.create_session()
+            news = db_sess.query(users.User).get(profile_id)
+            if not news:
+                return flask.jsonify({'error': 'Not found'})
+            return flask.jsonify(
+                {
+                    'profile': news.to_dict(only=(
+                        'id', 'username', 'about', 'email'))
+                }
+            )
+        except Exception as Error:
+            print(f'get_profile_info Error: {Error}')
+            return {'status': 'False'}
     else:
         print(f'This API request not available with not "GET" request')
+        return {'status': 'False'}
+
+
+# Удалить пользователя по id
+@blueprint.route('/api/profile/delete', methods=['POST'])
+def delete_profile():
+    if request.method == 'POST':
+        if not request.json:
+            return flask.jsonify({'error': 'Empty request'})
+        elif not all(key in request.json for key in
+                     ['token', 'id']):
+            return flask.jsonify({'error': 'Bad request'})
+
+        db_sess = db_session.create_session()
+        token = request.json['token']
+        user_id = request.json['id']
+        if token == super_user_token:
+            user = db_sess.query(users.User).get(id)
+            if not user:
+                return flask.jsonify({'error': 'user not found'})
+            else:
+                db_sess.delete(user)
+                db_sess.commit()
+                return flask.jsonify({'success': 'OK'})
+        else:
+            return {'error': 'invalid token'}
+    else:
+        print(f'This API request not available with not "POST" request')
         return {'status': 'False'}
--- server/config_server.py
+++ server/config_server.py
@@ -1,4 +1,5 @@
-
 global_settings = {
     'secret_key': 'qwerty123'
 }
+
+super_user_token = 'SKYMRG-34056-GFPRGE-364098-VGNLRSE'
