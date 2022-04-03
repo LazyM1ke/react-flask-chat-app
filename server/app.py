54391271908e3e7985b33f7647eb9597294ecf3c
MishaSok <mikhail.sokov2006@gmail.com>
2022-04-03 02:57:03 +0200

    - register handler added

--- server/app.py
+++ server/app.py
@@ -1,24 +1,35 @@
-from flask import Flask, request
+from flask import Flask, request, redirect
 from config_server import *
 from models import db_session
+from models.users import User
 
 app = Flask(__name__)
 
 app.config["SECRET_KEY"] = global_settings['secret_key']
 
 
-# Главная страница
-@app.route("/", methods=['GET'])
-def index():
-    pass
-
-
-@app.route("/kek", methods=['POST'])
+@app.route("/register", methods=['POST'])
 def register():
-    print(request.json())
+    if request.method == 'POST':
+        # Получаем json с данными
+        try:
+            db_sess = db_session.create_session()
+            req = request.get_json()
+            res = req['data']
+            user = User(
+                name=res['username'],
+                email=res['email'],
+            )
+            user.set_password(res['password'])
+            db_sess.add(user)
+            db_sess.commit()
+            return {'status': 'True'}
+        except Exception as Error:
+            print(f'Register error: {Error}')
+            return {'status': 'False'}
+
 
 
 if __name__ == "__main__":
     db_session.global_init('db/data_base.db')
     app.run(host="127.0.0.1", port=5000, debug=True)
-
