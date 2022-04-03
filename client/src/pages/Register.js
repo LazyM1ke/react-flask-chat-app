6d156ee424676c89bf9ef2d285d4a53a1809e419
Ivanov Mikhail <mishaivanov530@gmail.com>
2022-04-04 00:21:04 +0200

    - add validation

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,8 +1,10 @@
-import React, {useEffect, useState} from 'react';
+import React, {useState} from 'react';
 import {Link} from "react-router-dom";
 import './Register.scss';
-import {Button, TextField} from "@mui/material";
+import 'react-toastify/dist/ReactToastify.css';
+import { Button, Stack, TextField} from "@mui/material";
 import axios from "axios";
+import {toast, ToastContainer} from "react-toastify";
 
 const Register = () => {
     const [data, setData] = useState({
@@ -12,74 +14,115 @@ const Register = () => {
         confirmPassword: "",
     });
 
+    const toastOptions =  {
+        position: "bottom-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+    }
+
+
     const handleChange = (event) => {
         setData({ ...data, [event.target.name]: event.target.value });
     }
 
+    const handleValidation = () => {
+        const { password, confirmPassword, username, email } = data;
+        if (password !== confirmPassword) {
+            toast.error(
+                "Пароли должны совпадать !",
+                toastOptions
+            );
+            return false;
+        } else if (username.length < 3) {
+            toast.error(
+                "Имя пользователя должно быть болше 3 символов !",
+                toastOptions
+            );
+            return false;
+        } else if (password.length < 8) {
+            toast.error(
+                "Пароль должен быть >= 8 символов !",
+                toastOptions
+            );
+            return false;
+        } else if (email === "") {
+            toast.error("Эл.почта не может быть пустой", toastOptions);
+            return false;
+        }
+
+        return true;
+    };
+
     const handleSubmit = (event) => {
         event.preventDefault();
-        axios({
-            method: 'post',
-            url: '/reg',
-            data: {
-                data
-            },
-            headers: {
-                mode: 'no-cors'
-            }
-        }).then(res => console.log(res.data))
+       if (handleValidation()) {
+           axios({
+               method: 'post',
+               url: '/reg',
+               data: {
+                   data
+               },
+               headers: {
+                   mode: 'no-cors'
+               }
+           }).then(res => res.data.status == "True"
+               ? toast.success("Регистрация успешна ! ", toastOptions)
+               : toast.error("ХУЙ ТАМ ПЛАВАЛ",toastOptions))
+       }
     }
-
-    // const handleValidation = () => {
-    //     const {username, email, password, confirmPassword} = data
-    //
-    //     if (password !== confirmPassword) {
-    //
-    //     }
-    //
-    // }
 
 
     return (
-        <div className="form-container">
-            <form onSubmit={(event => handleSubmit(event))}>
+            <>
+                <ToastContainer />
+                <div className="form-container">
 
-                <h1>Регистрация</h1>
+                    <form onSubmit={(event => handleSubmit(event))}>
 
-                <TextField id="outlined-basic"
-                           label="Имя пользователя"
-                           name="username"
-                           variant="outlined"
-                           onChange={(e) => handleChange(e)}
-                />
-                <TextField id="outlined-basic"
-                           label="Эл. почта"
-                           name="email"
-                           variant="outlined"
-                           type="email"
-                           onChange={(e) => handleChange(e)}
-                />
-                <TextField id="outlined-basic"
-                           label="Пароль"
-                           name="password"
-                           variant="outlined"
-                           type="password"
-                           onChange={(e) => handleChange(e)}
-                />
-                <TextField id="outlined-basic"
-                           label="Подтверждение пароля"
-                           name="confirmPassword"
-                           variant="outlined"
-                           type="password"
-                           onChange={(e) => handleChange(e)}
-                />
+                        <h1>Регистрация</h1>
 
-                <Button variant="contained" onSubmit={handleSubmit} type='submit'>Создать аккаунт</Button>
+                        <TextField id="outlined-basic"
+                                   label="Имя пользователя"
+                                   name="username"
+                                   variant="outlined"
+                                   onChange={(e) => handleChange(e)}
+                        />
+                        <TextField id="outlined-basic"
+                                   label="Эл. почта"
+                                   name="email"
+                                   variant="outlined"
+                                   type="email"
+                                   onChange={(e) => handleChange(e)}
+                        />
+                        <TextField id="outlined-basic"
+                                   label="Пароль"
+                                   name="password"
+                                   variant="outlined"
+                                   type="password"
+                                   onChange={(e) => handleChange(e)}
+                        />
+                        <TextField id="outlined-basic"
+                                   label="Подтверждение пароля"
+                                   name="confirmPassword"
+                                   variant="outlined"
+                                   type="password"
+                                   onChange={(e) => handleChange(e)}
+                        />
 
-                <span>Уже есть аккаунт ? <Link to="/login"><Button variant="outlined" size="small">Войти</Button></Link></span>
+                        <Button variant="contained" onSubmit={handleSubmit} type='submit'>Создать аккаунт</Button>
 
-            </form>
-        </div>
+                        <span>Уже есть аккаунт ? <Link to="/login"><Button variant="outlined" size="small">Войти</Button></Link></span>
+
+                    </form>
+
+                </div>
+
+            </>
+
     );
 };
 
