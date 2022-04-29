1f7e21c869a678e6cc516b82bb2247365581dfbd
MishaSok <mikhail.sokov2006@gmail.com>
2022-04-30 00:57:16 +0200

    - socket debug

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 from requests import post, get
-
+import json
 # print(post('http://localhost:5000/reg',
 #            json={'title': 'Заголовок',
 #                  'content': 'Текст новости',
@@ -8,6 +8,10 @@ from requests import post, get
 
 
 print(post('http://127.0.0.1:5000/api/get_messages', json={'from': 'kekus',
-                                                          'to': 'mishasok',
+                                                           'to': 'mishasok',
                                                            }).json())
 
+a = {'messages': [{'content': 'test message', 'created_date': 'Sun, 17 Apr 2022 13:10:23 GMT', 'fromself': 'True'},
+                  {'content': 'test message', 'created_date': 'Sun, 17 Apr 2022 13:10:40 GMT', 'fromself': 'True'},
+                  {'content': 'jsdhfkjsdhf', 'created_date': 'Sun, 17 Apr 2022 13:57:30 GMT', 'fromself': 'True'},
+                  {'content': 'sjkdhfkjsdfh', 'created_date': 'Sun, 17 Apr 2022 13:57:52 GMT', 'fromself': 'True'}]}
