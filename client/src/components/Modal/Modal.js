399fd08ea9bd008a58066a9731e3c31dc79fed89
Ivanov Mikhail <mishaivanov530@gmail.com>
2022-09-04 15:19:00 +0200

    - kal

---
+++ client/src/components/Modal/Modal.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import "./Modal.scss";
+
+
+const Modal = ( ) => {
+
+    return (
+        <div className="modal">
+
+        </div>
+
+    );
+};
+
+export default Modal;
